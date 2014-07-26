ea72014d6ecccbfa1c7442508ecd6c0182aac752
mammut <irken.cs@gmail.com>
2014-07-25 23:34:00 -0400

    Added AngularJS for the frontend ofthe application

--- desktop/src/app.js
+++ desktop/src/app.js
@@ -27,3 +27,27 @@ var server = app.listen(3000, function(){
 server.on('close', function(){
 
 });
+
+var clients = [];
+app.post('/join', function(req, res){
+  clients.push({name: req.body.name, android_id: req.body.android_id});
+  window.location.href = 'index.html#/';
+  res.end();
+});
+
+app.get('/leave', function(req, res) {
+  var index;
+
+  for (var i = 0; i < clients.length; i++) {
+    if (clients.android_id == req.query.android_id) {
+      index = i; 
+      break;
+    }
+  }
+  console.log(index);
+
+  clients.splice(index, 1);
+
+  window.location.href = 'index.html#/';
+  res.end();
+});
