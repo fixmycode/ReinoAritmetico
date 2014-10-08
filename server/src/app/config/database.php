a6b5a54fe764b87a218b2b1cd81198e63ab9e70d
mammut <irken.cs@gmail.com>
2014-10-07 21:58:27 -0300

    Changed DB credentials for DO server

--- server/src/app/config/database.php
+++ server/src/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'reino-aritmetico',
+			'username'  => 'root',
+			'password'  => 'blackbird fly away',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
